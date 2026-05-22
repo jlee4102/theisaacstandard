baadc55be94d95dd59d14e2ebee3782ddc3a1573
James <philmedownn@gmail.com>
2026-05-22 09:08:41 -0400

    Add Q20i tweet card (1600x900) — for X thread Tweet 1 image

--- automation/svg-to-png.ps1
+++ automation/svg-to-png.ps1
@@ -32,6 +32,7 @@ $exportSizes = @{
     'twitter-banner.svg'    = @{ width = 1500; height = 500  }
     'web-banner.svg'        = @{ width = 1920; height = 480  }
     'video-intro.svg'       = @{ width = 1920; height = 1080 }
+    'q20i-x-card.svg'       = @{ width = 1600; height = 900  }
 }
 
 $wrapperTemplate = @'
